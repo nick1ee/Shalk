927dd9c8b618a8252cd6128fc3575a6872ec31bc
李崇維 <nicklee@lichongweis-MacBook-Pro.local>
2017-07-27 14:28:40 +0800

    Initial Commit

---
+++ Shalk/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Shalk
+//
+//  Created by Nick Lee on 2017/7/27.
+//  Copyright © 2017年 nicklee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
