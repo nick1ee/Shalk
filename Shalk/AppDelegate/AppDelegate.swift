a37d685b5d70a2fdd3f245b177cf471eac90d633
Nick Lee <nicklee@lichongweis-MacBook-Pro.local>
2017-07-27 16:39:45 +0800

    finish login ui page and init firebase

--- Shalk/AppDelegate/AppDelegate.swift
+++ Shalk/AppDelegate/AppDelegate.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import Firebase
+import IQKeyboardManagerSwift
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,7 +16,16 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+
+        UIApplication.shared.statusBarStyle = .lightContent
+
+        // Init Firebase
+//        FirebaseApp.configure()
+
+        // IQKeyboardManager
+        IQKeyboardManager.sharedManager().enable = true
+        IQKeyboardManager.sharedManager().shouldResignOnTouchOutside = true
+
         return true
     }
 
---
+++ Shalk/ViewController/LoginViewController.swift
@@ -0,0 +1,22 @@
+//
+//  LoginViewController.swift
+//  Shalk
+//
+//  Created by Nick Lee on 2017/7/27.
+//  Copyright © 2017年 nicklee. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    @IBOutlet weak var iconEmail: UIImageView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        iconEmail.tintColor = UIColor.lightGray
+
+    }
+
+}
