f2891b54da6c4b05282a8a2855a2165780d04547
Nick Lee <nicklee@lichongweis-MacBook-Pro.local>
2017-07-27 17:28:10 +0800

    add register page

--- Shalk/ViewController/RegisterViewController.swift
+++ Shalk/ViewController/RegisterViewController.swift
@@ -1,5 +1,5 @@
 //
-//  LoginViewController.swift
+//  RegisterViewController.swift
 //  Shalk
 //
 //  Created by Nick Lee on 2017/7/27.
@@ -8,29 +8,34 @@
 
 import UIKit
 
-class LoginViewController: UIViewController {
+class RegisterViewController: UIViewController {
+
+    @IBOutlet weak var iconUser: UIImageView!
 
     @IBOutlet weak var iconEmail: UIImageView!
+
     @IBOutlet weak var iconKey: UIImageView!
+
+    @IBOutlet weak var inputUserName: UITextField!
 
     @IBOutlet weak var inputEmail: UITextField!
 
     @IBOutlet weak var inputPassword: UITextField!
 
-    @IBAction func btnLogin(_ sender: UIButton) {
+    @IBAction func btnSignUp(_ sender: UIButton) {
 
     }
 
-    @IBAction func btnCreateNewAccount(_ sender: UIButton) {
+    @IBAction func btnBackToLogin(_ sender: UIButton) {
 
-        let registerVC = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "registerVC")
-
-        self.present(registerVC, animated: true, completion: nil)
+        self.dismiss(animated: true, completion: nil)
 
     }
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        iconUser.tintColor = UIColor.lightGray
 
         iconEmail.tintColor = UIColor.lightGray
 
