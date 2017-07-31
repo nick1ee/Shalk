801ea967405e69a241585ebc953a8e541fbe758f
Nick Lee <nicklee@lichongweis-MacBook-Pro.local>
2017-07-31 11:19:59 +0800

    finish design audio chat ui

--- Shalk/ViewController/AudioCallViewController.swift
+++ Shalk/ViewController/AudioCallViewController.swift
@@ -10,26 +10,69 @@ import UIKit
 
 class AudioCallViewController: UIViewController {
 
+    var isMicrophoneEnabled: Bool = true
+
+    var isSpeakerEnabled: Bool = false
+
+    @IBOutlet weak var userImageView: UIImageView!
+
+    @IBOutlet weak var defaultUserImageView: UIImageView!
+
+    @IBOutlet weak var userNameLabel: UILabel!
+
+    @IBOutlet weak var outletSpeaker: UIButton!
+
+    @IBOutlet weak var outletMicrophone: UIButton!
+
+    @IBAction func btnSpeaker(_ sender: UIButton) {
+
+        if isSpeakerEnabled {
+
+            // MARK: User muted the speaker
+
+            isSpeakerEnabled = false
+
+            outletSpeaker.setImage(UIImage(named: "icon-nospeaker.png"), for: .normal)
+
+        } else {
+
+            isSpeakerEnabled = true
+
+            outletSpeaker.setImage(UIImage(named: "icon-speaker.png"), for: .normal)
+
+        }
+
+    }
+
+    @IBAction func btnMicrophone(_ sender: UIButton) {
+
+        if isMicrophoneEnabled {
+
+            // MARK: User muted the local microphone
+
+            isMicrophoneEnabled = false
+
+            outletMicrophone.setImage(UIImage(named: "icon-nomic.png"), for: .normal)
+
+        } else {
+
+            // MARK: User enabled the local microphone
+
+            isMicrophoneEnabled = true
+
+            outletMicrophone.setImage(UIImage(named: "icon-mic.png"), for: .normal)
+
+        }
+
+    }
+
+    @IBAction func btnEndCall(_ sender: UIButton) {
+
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
