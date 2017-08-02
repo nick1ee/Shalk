f7c0101638df5ab286d4e766c7c6c1fdbd546036
Nick Lee <nicklee@lichongweis-MacBook-Pro.local>
2017-08-02 11:22:42 +0800

    add header

--- Shalk/ViewController/ProfileViewController.swift
+++ Shalk/ViewController/ProfileViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ProfileViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+
     @IBAction func btnModifyProfile(_ sender: UIBarButtonItem) {
 
     }
@@ -21,7 +23,9 @@ class ProfileViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-//        displayUserInfo()
+        tableView.estimatedRowHeight = 200
+
+        tableView.rowHeight = UITableViewAutomaticDimension
 
     }
 
@@ -80,6 +84,55 @@ extension ProfileViewController: UITableViewDelegate, UITableViewDataSource {
 
         }
 
+    }
+
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+
+        switch  section {
+
+        case 0:
+
+            return 0
+
+        default:
+
+            return 30
+
+        }
+
+    }
+
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+
+        let screenSize = UIScreen.main.bounds
+
+        if section == 1 {
+
+            let header = UIView(frame: CGRect(x: 0, y: 0, width: screenSize.width, height: 30))
+
+            header.backgroundColor = UIColor.init(red: 243/255, green: 174/255, blue: 47/255, alpha: 1)
+
+            let iconFriend = UIImageView.init(image: UIImage(named: "icon-friend.png"))
+
+            iconFriend.tintColor = UIColor.white
+
+            iconFriend.frame = CGRect(x: screenSize.width / 2 - 15, y: 2, width: 25, height: 25)
+
+            let friendAmountLabel = UILabel(frame: CGRect(x: screenSize.width / 2 + 10, y: 2, width: 25, height: 25))
+
+            friendAmountLabel.text = "10"
+
+            friendAmountLabel.textColor = UIColor.white
+
+            header.addSubview(iconFriend)
+
+            header.addSubview(friendAmountLabel)
+
+            return header
+
+        }
+
+        return nil
     }
 
 }
