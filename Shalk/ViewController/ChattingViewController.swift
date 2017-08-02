b13cb6377243472e63c2393d4cfef17138761007
Nick Lee <nicklee@lichongweis-MacBook-Pro.local>
2017-08-02 19:18:18 +0800

    commit for save status

---
+++ Shalk/ViewController/ChattingViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ChattingViewController.swift
+//  Shalk
+//
+//  Created by Nick Lee on 2017/8/2.
+//  Copyright © 2017年 nicklee. All rights reserved.
+//
+
+import UIKit
+import Firebase
+import JSQMessagesViewController
+
+final class ChattingViewController: JSQMessagesViewController {
+    
+//    var channelRef: DatabaseReference?
+//    
+//    var channel: Channel? {
+//        
+//        didSet {
+//            
+//            title = channel?.name
+//            
+//        }
+//        
+//    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
